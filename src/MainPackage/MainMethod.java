8f2d015131e951e07278df5a6c430ecfe02f8aaf
Abdulmalek Taher Ali Aldhafer <58888990+akaldhafer@users.noreply.github.com>
2021-06-20 06:11:00 +0400

    initial the project

---
+++ src/MainPackage/MainMethod.java
@@ -0,0 +1,17 @@
+package MainPackage;
+
+public class MainMethod {
+
+    public static void main(String[] args) throws Exception {  
+    
+               
+       Database data = new Database();
+       data.Database();
+          
+       LoginMenu ad = new LoginMenu();
+       ad.setLocationRelativeTo(null);            
+       ad.setVisible(true);
+    } 
+}
+    
+
